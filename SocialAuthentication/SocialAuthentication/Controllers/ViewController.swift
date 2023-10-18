23a9a6f71088fde1a690cf46b445c8fdf5add183
Zafran zaman <mzafransultani56@gmal.com>
2023-10-18 10:03:12 +0500

    Initial Setup Complete

---
+++ SocialAuthentication/SocialAuthentication/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialAuthentication
+//
+//  Created by Zafran Mac on 18/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
